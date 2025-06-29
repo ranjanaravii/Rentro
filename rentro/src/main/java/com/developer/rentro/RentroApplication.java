7b77d201bd1993d7daec6d72a851f83f8df7da47
raviranjankumar <raviranjan.kumar@lenskart.com>
2025-06-29 12:57:25 +0530

    initial commit

---
+++ rentro/src/main/java/com/developer/rentro/RentroApplication.java
@@ -0,0 +1,13 @@
+package com.developer.rentro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentroApplication.class, args);
+	}
+
+}
