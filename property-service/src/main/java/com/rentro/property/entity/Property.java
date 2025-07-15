d878044073031e051bc607aee53d3ff957eb76e1
raviranjankumar <raviranjan.kumar@lenskart.com>
2025-07-15 23:20:37 +0530

    code fix

--- property-service/src/main/java/com/rentro/property/entity/Property.java
+++ property-service/src/main/java/com/rentro/property/entity/Property.java
@@ -12,6 +12,9 @@ import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import lombok.Data;
+
+@Data
 @Entity
 @Table(name = "properties")
 @EntityListeners(AuditingEntityListener.class)
